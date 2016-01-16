2f510ce341374312785454dadbfbbea4a33596c1
Xiaocong <xiaocong@ualberta.ca>
2016-01-16 16:31:11 -0700

    Did nothing

--- JointLimitHandler/src/main/Main.java
+++ JointLimitHandler/src/main/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args){
 		
 		BvhFileReader bvhFileReader=new BvhFileReader();
-		bvhFileReader.setBvhFilePath("Hard code your BVH file path here.");
+		bvhFileReader.setBvhFilePath("C:\\Users\\xiaocong\\Desktop\\cmuconvert-daz-01-09\\01\\01_01.bvh");
 		bvhFileReader.getCurrentBvhFileDataAsObject();
 	}
 
